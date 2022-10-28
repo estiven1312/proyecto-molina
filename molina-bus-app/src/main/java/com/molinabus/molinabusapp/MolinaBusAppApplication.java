37748c441abe53edab3314382ef5504ded073f19
Estiven Hurtado <santosestiven46@gmail.com>
2022-10-27 22:10:30 -0500

    Inicializacion de repo

---
+++ molina-bus-app/src/main/java/com/molinabus/molinabusapp/MolinaBusAppApplication.java
@@ -0,0 +1,13 @@
+package com.molinabus.molinabusapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MolinaBusAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MolinaBusAppApplication.class, args);
+	}
+
+}
---
+++ molina-bus-app/src/test/java/com/molinabus/molinabusapp/MolinaBusAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.molinabus.molinabusapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MolinaBusAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
